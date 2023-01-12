b1b14fc9a6efb294012c59a6f896252010ab96bf
Luis127Florez <florezgilluisantony@gmail.com>
2023-01-12 16:47:09 -0500

    realizando cambios en el sql

--- appwowti.sql
+++ appwowti.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-01-2023 a las 21:44:23
+-- Tiempo de generación: 12-01-2023 a las 22:46:29
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -59,6 +59,13 @@ CREATE TABLE `compras` (
   `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `compras`
+--
+
+INSERT INTO `compras` (`id`, `idUser`, `total`, `estado`, `createdAt`, `updatedAt`) VALUES
+(1, 2, 91.08, 1, '2023-01-12 21:47:05', '2023-01-12 21:47:05');
+
 -- --------------------------------------------------------
 
 --
@@ -74,6 +81,14 @@ CREATE TABLE `detallecompra` (
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `detallecompra`
+--
+
+INSERT INTO `detallecompra` (`id_compra`, `cantidad`, `idMaquina`, `precioUnitario`, `importe`, `createdAt`, `updatedAt`) VALUES
+(1, 2, 1, 18.38, 36.76, '2023-01-12 22:29:28', '2023-01-12 22:29:28'),
+(1, 2, 2, 27.16, 54.32, '2023-01-12 22:32:44', '2023-01-12 22:32:44');
 
 -- --------------------------------------------------------
 
@@ -94,6 +109,14 @@ CREATE TABLE `maquinas` (
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `maquinas`
+--
+
+INSERT INTO `maquinas` (`id`, `descripcion`, `region`, `almacenamiento`, `sistemaOperativo`, `redes`, `complementos`, `duracionPlazo`, `precio`, `createdAt`, `updatedAt`) VALUES
+(1, 'Cloud VPS S', 'European Union (Germany)', '{tamaño: 200 , tipo:\"GB\" , disco:\"SSD\"}', 'Windows Server', '{PrivateNetworking:\"No Private Networking\",Bandwidth:\"32 tv out\", IPv4:\"1 Ip address\"}', '{ObjectStorage:\"none\", BackupSpace:\"none\", ServerManagement:\"unmanaged\",Monitoring:\"none\", sll:\"none\"}', '1 mes', 18.38, '2023-01-12 21:49:20', '2023-01-12 21:49:20'),
+(2, 'Cloud VPS S', 'United States (West)', '{tamaño: 400 , tipo:\"GB\" , disco:\"SSD\"}', 'ubuntu 4.0', '{PrivateNetworking:\"No Private Networking\",Bandwidth:\"32 tv out\", IPv4:\"1 Ip address\"}', '{ObjectStorage:\"500 GB Object Storage in United States\", BackupSpace:\"none\", ServerManagement:\"unmanaged\",Monitoring:\"none\", sll:\"none\"}', '1 mes', 27.16, '2023-01-12 21:49:20', '2023-01-12 21:49:20');
 
 -- --------------------------------------------------------
 
@@ -177,13 +200,13 @@ ALTER TABLE `asignacions`
 -- AUTO_INCREMENT de la tabla `compras`
 --
 ALTER TABLE `compras`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `maquinas`
 --
 ALTER TABLE `maquinas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`

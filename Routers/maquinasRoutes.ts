c61734bd101e851a306cd43e01319ccc7591ed18
Jnajera <=>
2023-01-19 08:38:39 -0500

    quedó lista todas las funciones de traer las maquinas

--- Routers/maquinasRoutes.ts
+++ Routers/maquinasRoutes.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { getAllProductMaquinas, getMaquina, getMaquinas, pacthMaquina } from "../Controllers/maquinasControls";
+import { getAllProductMaquinas, getMaquina, pacthMaquina } from "../Controllers/maquinasControls";
 
 const maquinasRoutes = Router();
 
-maquinasRoutes.get('/', getMaquinas);
 maquinasRoutes.get('/products', getAllProductMaquinas);
 maquinasRoutes.get('/:id', getMaquina);
 maquinasRoutes.patch('/:id',pacthMaquina);
